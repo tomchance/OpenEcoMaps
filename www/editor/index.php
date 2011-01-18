b277187943905de5aa8ed3e481ed825c026c62a1
Tom Chance <tom@acrewoods.net>
2011-01-18 07:43:52 -0800

    Editor fixes and adding feature icons

--- www/editor/index.php
+++ www/editor/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <head> 
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" /> 
-  <link rel="stylesheet" type="text/css" href="../css/oem_style.css"> 
+  <link rel="stylesheet" type="text/css" href="../css/site_style.css"> 
 <script type="text/javascript" src="swfobject.js"></script>
   <title>OpenEcoMaps :: editor</title> 
 </head> 
