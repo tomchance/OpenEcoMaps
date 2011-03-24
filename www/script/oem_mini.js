af0acc9b1b28e29c2930ccb7b00ea96f25a0a261
tomchance <telex4@yahoo.com>
2011-03-24 13:15:56 +0000

    Changing KML URLs

--- www/script/oem_mini.js
+++ www/script/oem_mini.js
@@ -30,7 +30,7 @@ function initMap(lat, lon, zoom){
   var layerPower = addKMLLayer("Low carbon power", "http://www.openecomaps.co.uk/kml/one_planet_london/low_carbon_power.kml");
   var layerFood = addKMLLayer("Food", "http://www.openecomaps.co.uk/kml/one_planet_london/food.kml");
   var layerCulture = addKMLLayer("Culture and heritage", "http://www.openecomaps.co.uk/kml/one_planet_london/culture.kml");
-  var layersPOI = [layerPower, layerWaste, layerFood, layerTransport, layerCulture];
+  var layersPOI = [layerPower, layerFood, layerCulture];
   map.addLayers(layersPOI);
   var selectControl = new OpenLayers.Control.SelectFeature(layersPOI, {onSelect: onFeatureSelect, onUnselect: onFeatureUnselect});
   map.addControl(selectControl);
